844dc50d6f5302036cf04343197ba0b572c43688
Ruben Cardoza <rubencardoza611@gmail.com>
2023-03-09 19:07:30 -0700

    se quitaron cosas no necesarias de front y se agrego funcionalidad de buscar facturas por fecha y folio fiscal (separadas)

--- FacturamaConsumirApi/Controllers/HomeController.cs
+++ FacturamaConsumirApi/Controllers/HomeController.cs
@@ -2,10 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Services.Description;
+using System.Web.UI;
+using System.Web.UI.WebControls;
 using ApiFacturamaTest.Models;
+using Microsoft.Ajax.Utilities;
 using Newtonsoft.Json;
 
 namespace FacturamaConsumirApi.Controllers
@@ -14,25 +19,60 @@ namespace FacturamaConsumirApi.Controllers
 	{
 		public async  Task<ActionResult> Index()
 		{
-			string servicio = "https://localhost:44323/api/cdfi/";
+			string servicio = "https://localhost:44323/api/cdfi";
 			var httpClient = new HttpClient();
 			var json = await httpClient.GetStringAsync(servicio);
 			var listaFacturas = JsonConvert.DeserializeObject<List<FacturaModel>>(json);
+			
 			return View(listaFacturas);
 		}
 
-		public ActionResult About()
+		public async Task<ActionResult> folioFiscalini(string folioFiscal)
 		{
-			ViewBag.Message = "Your application description page.";
+			string servicio = "https://localhost:44323/api/CdfiByFolio/"+ folioFiscal;
+			var httpClient = new HttpClient();
+			var json = await httpClient.GetStringAsync(servicio);
+			var listaFacturas = JsonConvert.DeserializeObject<FacturaModel>(json);
 
-			return View();
+			return View(listaFacturas);
+			//Response.Write("<script>alert('" + folioFiscal + "')</script>");
+
+		}
+		public async Task<ActionResult> FacturasFolio(string FolioFiscal)
+		{
+
+			string servicio = "https://localhost:44323/api/CdfiByFolio/"+ FolioFiscal;
+			//Response.Write("<script>alert('" + servicio + "')</script>");
+			var httpClient = new HttpClient();
+			var json = await httpClient.GetStringAsync(servicio);
+			var factura = JsonConvert.DeserializeObject<FacturaModel>(json);
+			List<FacturaModel> listaFacturas = new List<FacturaModel> { factura };
+		
+
+			return View ("Index", listaFacturas);
+
+		}
+		public async Task<ActionResult> FacturasMultiFiltro(string fechaInicio,string fechaFin)
+		{
+
+			string servicio = "https://localhost:44323/api/CdfiMultiFiltro/?fechaInicio=" + fechaInicio + "&fechaFin=" + fechaFin;
+			//Response.Write("<script>alert('" + fechaInicio+fechaFin + "')</script>");
+			var httpClient = new HttpClient();
+			var json = await httpClient.GetStringAsync(servicio);
+			var listaFacturas = JsonConvert.DeserializeObject<List<FacturaModel>>(json);
+			return View("Index",listaFacturas);
+			
 		}
 
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
 
-			return View();
-		}
+
+
+
+
+
+
+
+
+
 	}
 }
---
+++ FacturamaConsumirApi/Scripts/JavaScript.js
@@ -0,0 +1,6 @@
+﻿if (document.getElementById('radioBtnFecha').checked) {
+    //Male radio button is checked
+
+} else if (document.getElementById('radioBtnFolio').checked) {
+    //Female radio button is checked
+}
