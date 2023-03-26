091deabc4a0c34526e07a1ddaa50ba64743470c6
brayan-alexis <brayanzxd@gmail.com>
2023-03-25 23:49:40 -0700

    [Update] Views, Styles y Scripts.

--- FacturamaConsumirApi/Scripts/JavaScript.js
+++ FacturamaConsumirApi/Scripts/JavaScript.js
@@ -6,7 +6,8 @@ var inputRfcEmisor = document.getElementById("rfcEmisor");
 var inputSerie = document.getElementById("serie");
 var inputRfcReceptor = document.getElementById("rfcReceptor");
 var inputFolio = document.getElementById("folio");
-//funciones para habiliar los inputs de fecha y folio
+
+// Funciones para habiliar los inputs de fecha y folio
 function enableFolioFiscal() {
 	inputFolioFiscal.disabled = "";
 	inputFechaInicio.disabled = "true";
@@ -15,7 +16,19 @@ function enableFolioFiscal() {
 	inputSerie.disabled = "true";
 	inputRfcReceptor.disabled = "true";
 	inputFolio.disabled = "true";
+
+	inputFolioFiscal.classList.remove("disabled-input");
+	inputFechaInicio.classList.add("disabled-input");
+	inputFechaFin.classList.add("disabled-input");
+	inputRfcEmisor.classList.add("disabled-input");
+	inputSerie.classList.add("disabled-input");
+	inputRfcReceptor.classList.add("disabled-input");
+	inputFolio.classList.add("disabled-input");
+
+	inputFechaInicio.style.cursor = "";
+	inputFechaFin.style.cursor = "";
 }
+
 function enableFechasEmision() {
 	inputFolioFiscal.disabled = "true";
 	inputFechaInicio.disabled = "";
@@ -24,4 +37,15 @@ function enableFechasEmision() {
 	inputSerie.disabled = "";
 	inputRfcReceptor.disabled = "";
 	inputFolio.disabled = "";
+
+	inputFolioFiscal.classList.add("disabled-input");
+	inputFechaInicio.classList.remove("disabled-input");
+	inputFechaFin.classList.remove("disabled-input");
+	inputRfcEmisor.classList.remove("disabled-input");
+	inputSerie.classList.remove("disabled-input");
+	inputRfcReceptor.classList.remove("disabled-input");
+	inputFolio.classList.remove("disabled-input");
+
+	inputFechaInicio.style.cursor = "pointer";
+	inputFechaFin.style.cursor = "pointer";
 }
