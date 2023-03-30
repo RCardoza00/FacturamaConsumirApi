a5583bd6bdfc63cc34b18956aaa6075b4298735e
brayan-alexis <brayanzxd@gmail.com>
2023-03-29 22:51:02 -0700

    Validaciones en EmisorModel

--- FacturamaConsumirApi/Models/EmisorModel.cs
+++ FacturamaConsumirApi/Models/EmisorModel.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Net.Http;
+using System.Resources;
 using System.Web;
+using System.Web.Mvc;
 
 namespace FacturamaConsumirApi.Models
 {
@@ -11,7 +14,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo RFC del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo RFC Emisor es obligatorio.")]
         [StringLength(50)]
         [Display(Name = "RFC Emisor")]
         public string EmisorRfc { get; set; }
@@ -19,7 +22,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo Razón Socail del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Razón Social es obligatorio.")]
         [StringLength(500)]
         [Display(Name = "Razón Social")]
         public string EmisorRazSocial { get; set; }
@@ -27,7 +30,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo Estatus del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Estatus es obligatorio.")]
         [StringLength(50)]
         [Display(Name = "Estatus")]
         public string EmisorEstatus { get; set; }
@@ -35,7 +38,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo Regimen Fiscal del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Régimen Fiscal es obligatorio.")]
         [StringLength(50)]
         [Display(Name = "Régimen Fiscal")]
         public string EmisorRegFiscal { get; set; }
@@ -64,7 +67,10 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo Correo del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Correo es obligatorio.")]
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,}|[0-9]{1,3})(\]?)$",
+            ErrorMessage = "El correo electrónico ingresado no es válido.")]
+        //[EmailAddress(ErrorMessage = "El correo electrónico ingresado no es válido.")] ALTERNATIVA SIMPLE -> Acepta juan@juan como valida
         [StringLength(100)]
         [Display(Name = "Correo")]
         public string EmisorCorreo { get; set; }
@@ -79,7 +85,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo de Código Postal del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Código Postal es obligatorio.")]
         [StringLength(20)]
         [Display(Name = "Código Postal")]
         public string EmisorCodPostal { get; set; }
@@ -87,7 +93,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo de Municipio del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Municipio es obligatorio.")]
         [StringLength(100)]
         [Display(Name = "Municipio")]
         public string EmisorMunicipio { get; set; }
@@ -95,7 +101,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo de Estado del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Estado es obligatorio.")]
         [StringLength(50)]
         [Display(Name = "Estado")]
         public string EmisorEstado { get; set; }
@@ -103,7 +109,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo de Colonia del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Colonia es obligatorio.")]
         [StringLength(100)]
         [Display(Name = "Colonia")]
         public string EmisorColonia { get; set; }
@@ -111,7 +117,7 @@ namespace FacturamaConsumirApi.Models
         /// <summary>
         /// Atributo de Calle del Emisor
         /// </summary>
-        [Required]
+        [Required(ErrorMessage = "El campo Calle es obligatorio.")]
         [StringLength(100)]
         [Display(Name = "Calle")]
         public string EmisorCalle { get; set; }
@@ -120,7 +126,7 @@ namespace FacturamaConsumirApi.Models
         /// Atributo de Número Exterior del Emisor
         /// </summary>
         [StringLength(30)]
-        [Display(Name = "No. Exterior")]
+        [Display(Name = "No. Exterior")]    
         public string EmisorNoExterior { get; set; }
 
         /// <summary>
